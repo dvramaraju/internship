2c4c7b147e03b0066237f5c3187b59e2a00d277d
dvramaraju <ramarajudv21@gmail.com>
2021-10-27 19:36:26 +0530

    sign in and dashboard changes

--- src/containers/SignIn.tsx
+++ src/containers/SignIn.tsx
@@ -28,7 +28,7 @@ export const SignIn = () => {
   function handleSubmit(event: any) {
     event.preventDefault();
     axios.get(`http://localhost:8000/users?email=${event.target.value}`).then((value: any) => {
-      value && value !== loginDetails.email ? history.push("/") : alert("User not exist")
+      value && value !== loginDetails.email ? history.push("/dashboard") : alert("User not exist")
     })
   }
 
